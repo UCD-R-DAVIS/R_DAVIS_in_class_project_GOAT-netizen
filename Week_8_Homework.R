717bcf2a01c762bf65136a4663097983504c2452
Alex Sanchez1 <alexsanchez1@campus-023-163.ucdavis.edu>
2024-11-21 13:45:47 -0800

    additonal edits

--- Week_8_Homework.R
+++ Week_8_Homework.R
@@ -1,15 +1,17 @@
 ## Data Import
+library(tidyverse)
+library(lubridate)
 mloa <- read_csv("https://raw.githubusercontent.com/gge-ucd/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
 str(mloa)
 summary(mloa)
-library(tidyverse)
-library(lubridate)
+
+head(mloa)
 
 mloa2 = mloa %>%
   # Remove NA's
   filter(rel_humid != -99) %>%
   filter(temp_C_2m != -999.9) %>%
-  filter(windSpeed_m_s != -999.9) %>%
+  filter(windSpeed_m_s != -999.99) %>%
   mutate(datatime = ymd_hm(paste0(year, "-",
                                   month, "-",
                                   day< ",",
@@ -19,7 +21,10 @@ mloa2 = mloa %>%
 mutate(datetimeLocal = with_tz(datatime, tz = "Pacific/Honolulu"))
 
 ## Plot
+library(lubridate)
 
+mloa %>% mutate(hour_hst = hour(datetime_hst)) %>% group_by(hour_hst,month) %>%
+  
 mloa %>%
   mutate(localMon = month(datetimeLocal, label = TRUE),
          localHour = hour(datetimeLocal)) %>%
@@ -31,5 +36,5 @@ mloa %>%
   scale_color_viridis_c() +
   xlab("Month") +
   ylab("Mean temperature (degrees C)") +
-  theme_classic()
+  theme_gray()
   
